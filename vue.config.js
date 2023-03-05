93abe956137a806cb412cd2c069d7e07244e6c46
falaqin <falaqin.work@gmail.com>
2023-03-05 04:18:09 +0000

    - fix AGAIN

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'https://falaqin.xyz/'
+  publicPath: 'http://falaqin.xyz/'
 })
